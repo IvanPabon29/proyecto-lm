3ee33c20ad7264af68bec73f51468660aaa4c510
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2024-08-10 19:52:52 -0500

    Creacion Api producto

---
+++ src/api/productoApi.js
@@ -0,0 +1,26 @@
+// src/api/productoApi.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api';
+
+// Registrar Productos
+export const registrarProducto = async (formData) => {
+  try {
+    const response = await axios.post(`${API_URL}/registro-entrada/nuevo-producto`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' }
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response ? error.response.data.message : error.message);
+  }
+};
+
+// Obtener Productos 
+export const obtenerProductos = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/productos`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response ? error.response.data.message : error.message);
+  }
+};
