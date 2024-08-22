e251c075eb2c13b504ce72c9ba96f3e135e6d2cd
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2024-08-22 14:10:00 -0500

    Modificar a Responsive Se agrega un div para el overlay y otro para el contenido

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,27 +2,39 @@ import "../styles/Home.css";
 import Video from "../video/Fondo_video.mp4";
 
 function Home() {
-  return(
+  return (
     <section id="inicio">
-      
-      <video autoPlay muted loop src={Video} ></video>
+      <div className="overlay">
+        <video autoPlay muted loop src={Video}></video>
 
-      <h2>Lujos y Repuestos L&M</h2>
+        <div className="contenido">
+          <h2>Lujos y Repuestos L&M</h2>
 
-      {/*  mision */}
-      <div className="mision">
-        <h3>Mision</h3>
-        <p>Nos especializamos en la venta de repuestos y lujos para motocicletas, Entregando excelente servicio, mejores productos y siendo competitivo en calidad y precios. Enviando productos al todo el país</p>
+          {/* Misión */}
+          <div className="mision">
+            <h3>Misión</h3>
+            <p>
+              Nos especializamos en la venta de repuestos y lujos para
+              motocicletas, entregando excelente servicio, los mejores productos
+              y siendo competitivos en calidad y precios. Enviamos productos a
+              todo el país.
+            </p>
+          </div>
+
+          {/* Visión */}
+          <div className="vision">
+            <h3>Visión</h3>
+            <p>
+              Dentro de 10 años ser la mejor empresa importadora y
+              comercializadora de repuestos y lujos en Colombia, brindando un
+              excelente servicio en venta y post-venta, generando valor agregado
+              para nuestros clientes.
+            </p>
+          </div>
+        </div>
       </div>
-
-        {/* vision */}
-      <div className="vision">
-        <h3>Vision</h3>
-        <p>Dentro de 10 años ser la mejor empresa importadora y comercializadora de repuestos y lujos en Colombia, brindando un excelente servicio en venta y post venta, generando valor agregado para nuestros clientes</p>
-      </div>
-
     </section>
   );
-};
+}
 
 export default Home;
