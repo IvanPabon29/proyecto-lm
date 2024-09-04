5cf2b965d4bc0d78bd102b825e4bae6f34c7d153
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2024-09-03 20:02:35 -0500

    Modificar userContext Se agrego localStorage para guarda al usuario y cuando se recargar la pagina No desaparezca la session del usuario

--- src/pages/userContext.js
+++ src/pages/userContext.js
@@ -1,9 +1,22 @@
-import { createContext, useContext, useState } from 'react';
+//src\pages\userContext.js
+
+import { createContext, useContext, useState, useEffect } from 'react';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
