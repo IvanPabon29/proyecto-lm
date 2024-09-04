c3b8a0c29564763f33cbc48bbe30e6039e2edcea
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2024-09-03 20:05:07 -0500

    Modificar Login se modifico el login para asegurar que el contexto se guarde correctamente

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,14 +12,13 @@ function Login() {
   // Manejador del login.
   const handleLogin = async (e) => {
     e.preventDefault();
-
-    // Acceder a los valores de los inputs.
+  
     const usuario = e.target.elements.usuario.value;
     const contraseña = e.target.elements.contraseña.value;
-
+  
     try {
       const data = await loginUsuario(usuario, contraseña);
-
+  
       if (data.message === 'Login Exitoso') {
         setUser(data.user); // Guardar los datos del usuario en el contexto
         navegacion('/home'); // Redirige a la página principal
@@ -30,7 +29,7 @@ function Login() {
       console.error('Error:', error);
       alert('Error!, Usuario o Contraseña Incorrecta, Intente de Nuevo.');
     }
-  };
+  };  
 
   return (
     <section id="login">
