d418d998e719a65298dfbf47c04dde417e0a7406
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2024-09-17 19:19:29 -0500

    Agregar controlador obtenerRegistros se agrego para poder realizar la consulta de los registros de los productos

--- src/backend/controllers/registrosController.js
+++ src/backend/controllers/registrosController.js
@@ -18,12 +18,42 @@ const registrosController = {
       (err, results) => {
         if (err) {
           console.error("Error al registrar la venta:", err.message);
-          return res.status(500).send({ message: "Error al registrar la venta", error: err });
+          return res
+            .status(500)
+            .send({ message: "Error al registrar la venta", error: err });
         }
         res.status(201).json({ message: "Venta registrada exitosamente" });
       }
     );
   },
+
+  // Controlador para obtener registros filtrados por fecha
+  obtenerRegistros: (req, res) => {
+    const { fechaInicio, fechaFin } = req.query;
+
+    if (!fechaInicio || !fechaFin) {
+      return res
+        .status(400)
+        .json({ message: "Debe proporcionar las fechas de inicio y fin" });
+    }
+
+    const query = `
+      SELECT id_producto, nombre, modelo, fecha_registro, cantidad, tipo_accion 
+      FROM registros
+      WHERE fecha_registro BETWEEN ? AND ?
+    `;
+
+    db.query(query, [fechaInicio, fechaFin], (err, results) => {
+      if (err) {
+        console.error("Error al obtener los registros:", err.message);
+        return res
+          .status(500)
+          .json({ message: "Error al obtener los registros", error: err });
+      }
+      res.status(200).json(results);
+    });
+  },
+  
 };
 
 module.exports = registrosController;
